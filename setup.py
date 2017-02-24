61769030e377d0fecbc08ed7ab986f701b5f9533
Rolando Espinoza <rndmax84@gmail.com>
2017-02-23 22:09:04 -0400

    Bump version and update author info.

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ LONG_DESC = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name='scrapydo',
-    version='0.1.0',
+    version='0.2.0',
     description='Crochet-based blocking API for Scrapy.',
     long_description=LONG_DESC,
     author='Rolando Espinoza La fuente',
-    author_email='darkrho@gmail.com',
-    url='https://github.com/darkrho/scrapydo',
+    author_email='rndmax84@gmail.com',
+    url='https://github.com/rolando/scrapydo',
     license='MIT',
     packages=find_packages(),
     install_requires=[
